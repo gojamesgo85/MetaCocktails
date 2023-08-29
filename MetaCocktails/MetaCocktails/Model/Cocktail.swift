562ec199be009ec0e154f60f9017388b47e23e25
Matthew Hunt <matthew.hunt@wundermanthompson.com>
2023-08-29 16:50:13 -0600

    added a few weird cocktail recipes to test

--- MetaCocktails/MetaCocktails/Model/Cocktail.swift
+++ MetaCocktails/MetaCocktails/Model/Cocktail.swift
@@ -58,3 +58,94 @@ struct MockData: Identifiable {
                                 garnish: Garnish.orangePeel.rawValue)
 }
 
+/// Some weird cocktails:
+
+//Pornstar Martini
+
+//* 1.5 oz Vodka 
+//* 0.5 oz Giffard Passion Fruit Liqueur 
+//* 0.75 oz Lime Juice 
+//* 0.5 oz Vanilla syrup 
+//* 0.25 oz Passion Fruit syrup 
+//
+//Method: Shake
+//Glassware: Coupe
+//Garnish: 2 oz of Sparkling Wine in shot glass on side.
+//Origin: Douglas Ankrah, Townhouse Bar, London, Early 00s
+
+
+//El Guapo
+//
+//* 2 oz Blacno Tequila 
+//* 1/2 lime quartered 
+//* 0.75 oz Simple Syrup 
+//* 1 tsp Cholula 
+//
+//method: muddle / shake / dump
+//glassware: double old fashioned
+//garnish: salt and cracked black pepper
+
+
+//Gordon’s Cup
+//
+//* 2 oz London Dry Gin 
+//* 1/2 Lime quartered 
+//* 0.75 oz Simple Syrup 
+//* 3 slices cucumber 
+//
+//method: muddle / shake / dump
+//glassware: double old fashioned
+//garnish: salt and cracked black pepper
+
+
+//Breakfast Martini
+//
+//* 1.5 oz London Dry Gin 
+//* 0.5 oz Lemon Juice 
+//* 0.5 oz Cointreau 
+//* 1 tsp Orange marmalade 
+//
+//method: shake / fine strain
+//glassware: nick & nora
+//garnish: orange twist
+
+
+//Zombie
+//
+//* 1 oz 151pf Demerara Rum 
+//* 1 oz Overproof Jamaican Rum 
+//* 1 oz White Rum 
+//* 1 oz Don’s Mix 
+//* 0.75 oz Lime 
+//* 0.5 oz Velvet Falernum 
+//* 0.25 oz Grenadine 
+//* 2 dash Absinthe 
+//* 1 dash angostura 
+//
+//Method: short shake / strain
+//Glass: tiki mug
+//Garnish: mint / orchid
+
+
+//Queen's Park Swizzle
+//
+//* 2 oz Aged Demerara Rum  
+//* 1 oz Lime Juice  
+//* 0.75 oz Simple Syrup  
+//* 10 mint leaves  
+//
+//Method: whip / dump / ice / layer Angostura / Peychaud's Bitters
+//Glass:  pilsner / crushed ice
+//Garnish:  mint sprig
+
+
+//Ti Punch
+//
+//* 2 oz Rhum Agricole Blanc 
+//* 1 tsp Cane syrup 
+//* 1 lime disc, with flesh 
+//
+//Method: built in glass: muddle lime disc with syrup / add rum / add cracked ice / stir
+//Glass: single old fashioned
+//Garnish: none
+
